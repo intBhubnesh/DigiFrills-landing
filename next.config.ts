6229b2c8bacd012c6079f500977615f9ff4a3ef6
Bommaraju Sivani <sivani@Bommarajus-MacBook-Air.local>
2025-04-08 18:19:19 +0530

    testimonial card layout updates

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,12 @@ module.exports = {
   },
 
 };
+<<<<<<< HEAD
+=======
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+>>>>>>> cc8fd3b (Improved testimonial card styles)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+<<<<<<< HEAD
 
 "use client";
 import Navbar from "../sections/Navbar";
 import Hero from "../sections/Hero";
 import BenefitsCarousel from "../sections/BenefitsCarousel";
+=======
+import Image from "next/image";
+import Testimonial from '@/app/Components/Testimonials';
+>>>>>>> cc8fd3b (Improved testimonial card styles)
 
 export default function Home() {
   return (
@@ -11,7 +16,7 @@ export default function Home() {
       <Hero />
       <BenefitsCarousel />
     </div>
-    <Reviews />
+    <Testimonial />
     </>
   );
 }
