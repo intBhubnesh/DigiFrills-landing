ec648992813453872896ef168e687bfcb58e089b
Bommaraju Sivani <sivani@Bommarajus-MacBook-Air.local>
2025-04-11 10:32:20 +0530

    Resolved errors and cleaned up warnings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Navbar from "../sections/Navbar";
 import Hero from "../sections/Hero";
 import BenefitsCarousel from "../sections/BenefitsCarousel";
 
-import Image from "next/image";
+// import Image from "next/image";
 import Testimonial from '@/app/Components/Testimonials';
 
 
