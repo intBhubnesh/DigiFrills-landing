1449cb16fe7e7afb43507bf19fae769fd7d19cee
Bommaraju Sivani <sivani@Bommarajus-MacBook-Air.local>
2025-04-11 10:18:55 +0530

    Add testimonial component and update page.tsx

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,13 @@
+import type { NextConfig } from "next";
 
 module.exports = {
   images: {
-    domains: ["framerusercontent.com"],
+    domains: ['framerusercontent.com'],
   },
-
 };
-<<<<<<< HEAD
-=======
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
 export default nextConfig;
->>>>>>> cc8fd3b (Improved testimonial card styles)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
-<<<<<<< HEAD
+
 
 "use client";
 import Navbar from "../sections/Navbar";
 import Hero from "../sections/Hero";
 import BenefitsCarousel from "../sections/BenefitsCarousel";
-=======
+
 import Image from "next/image";
 import Testimonial from '@/app/Components/Testimonials';
->>>>>>> cc8fd3b (Improved testimonial card styles)
+
 
 export default function Home() {
   return (
+    <>
     <div>
       <Navbar />
       <Hero />
