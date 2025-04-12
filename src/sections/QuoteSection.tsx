bd5c0f418303a010efd973aab313070460d45fe4
Prithvinew004 <prithvi.new004@gmail.com>
2025-04-12 16:31:04 +0530

    Fixed the hero animation and quote alignment

--- src/sections/QuoteSection.tsx
+++ src/sections/QuoteSection.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const QuoteSection = () => {
   return (
     <section className="relative bg-[#F5F6F8] py-20 px-4 md:px-8">
-      <div className="max-w-7xl mx-auto flex flex-col lg:flex-row justify-between items-center gap-12">
+      <div className="max-w-7xl mx-auto flex flex-col lg:flex-row justify-between items-center gap-12 translate-x-24">
         {/* Orange Card */}
         <div className="bg-[#F45A36] rounded-[48px] w-full lg:w-[950px] h-auto lg:h-[650px] p-8 md:p-12 text-white relative">
           <div className="mt-6 md:mt-20">
@@ -18,7 +18,7 @@ const QuoteSection = () => {
         </div>
 
         {/* Form Card */}
-        <div className="bg-black text-white rounded-[40px] py-8 px-6 md:px-10 w-full max-w-lg shadow-xl">
+        <div className="bg-black text-white rounded-[40px] py-8 px-6 md:px-10 w-full max-w-lg shadow-xl absolute right-0">
           <form className="space-y-4">
             <input
               type="text"
